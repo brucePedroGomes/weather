a4129f29543d8a55310baf7e24f3f41e54befec9
Pedro Gomes <pedroleinar@gmail.com>
2022-03-31 20:00:20 -0300

    feat: request location

--- src/components/NeedLocation.tsx
+++ src/components/NeedLocation.tsx
@@ -1,8 +1,29 @@
 import { Heading, VStack } from "@chakra-ui/react";
+import { useAtom } from "jotai";
 import { ReactNode, useEffect, useState } from "react";
+import { locationAtom } from "../atoms";
 
 export function NeedLocation({ children }: { children: ReactNode }) {
   const [permission, setPermission] = useState<PermissionState>("denied");
+
+  const [location, setLocation] = useAtom(locationAtom);
+
+  useEffect(() => {
+    function success(position: GeolocationPosition) {
+      setLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    }
+
+    function error(error: GeolocationPositionError) {
+      return console.error(error);
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }, [setLocation]);
 
   useEffect(() => {
     async function getPermission() {
@@ -16,7 +37,7 @@ export function NeedLocation({ children }: { children: ReactNode }) {
     getPermission();
   }, []);
 
-  if (permission === "granted") {
+  if (location.latitude || permission === "granted") {
     return <>{children}</>;
   }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,9 @@
 import { Divider, VStack } from "@chakra-ui/react";
 
-import { useEffect } from "react";
-import { useAtom } from "jotai";
-import { locationAtom } from "../atoms";
-
 import { CurrentWeather } from "../components/";
 import { DailyWeather } from "../components/DailyWeather";
 
 export function Home() {
-  const [, setLocation] = useAtom(locationAtom);
-
-  useEffect(() => {
-    function success(position: GeolocationPosition) {
-      setLocation({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-      });
-    }
-
-    function error(error: GeolocationPositionError) {
-      return console.error(error);
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error);
-  }, [setLocation]);
-
   return (
     <VStack bgColor="gray.700" borderRadius="30px" p="6">
       <CurrentWeather />
