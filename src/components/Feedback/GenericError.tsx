78cd21c29706c7e3a0c6b6ea3ccadc82d7446b1c
Pedro Gomes <pedroleinar@gmail.com>
2022-03-31 01:27:58 -0300

    feat:  responsive generic error

--- src/components/Feedback/GenericError.tsx
+++ src/components/Feedback/GenericError.tsx
@@ -1,9 +1,12 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Icon, Text } from "@chakra-ui/react";
+import { BiErrorAlt } from "react-icons/bi";
 
 export function GenericError() {
   return (
     <Flex flexDir="column" align="center" w="full">
-      <Text>Oops! Something went wrong. Please try again later</Text>
+      <Icon as={BiErrorAlt} boxSize="36" />
+      <Text fontSize={["2xl", "3xl"]}>Oops! Something went wrong.</Text>
+      <Text fontSize={["xl", "2xl"]}>Please try again later.</Text>
     </Flex>
   );
 }
